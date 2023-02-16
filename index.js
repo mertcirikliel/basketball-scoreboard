5dc73b5b3f409d3ce8245b3172db5eb72c259f59
mertcirikliel <78266101+mertcirikliel@users.noreply.github.com>
2023-02-16 03:04:39 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+let newGame = document.getElementById("reset")
+
+let hScore = 0
+let gScore = 0
+
+function addOneHome() {
+    hScore = hScore + 1
+    homeScore.textContent = hScore
+}
+
+function addTwoHome() {
+    hScore = hScore + 2
+    homeScore.textContent = hScore
+}
+
+function addThreeHome() {
+    hScore = hScore + 3
+    homeScore.textContent = hScore
+}
+
+function addOneGuest() {
+    gScore = gScore + 1
+    guestScore.textContent = gScore
+}
+
+function addTwoGuest() {
+    gScore = gScore + 2
+    guestScore.textContent = gScore
+}
+
+function addThreeGuest() {
+    gScore = gScore + 3
+    guestScore.textContent = gScore
+}
+
+function reset() {
+    homeScore.textContent = 0
+    guestScore.textContent = 0
+    gScore = 0
+    hScore = 0
+}
